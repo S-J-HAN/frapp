948f547b963d56c14d12dc9266fae73956e46245
cpannier <charlotte@pannier.com.au>
2019-09-10 20:49:11 +1000

    Language selection done (need to add prompt message) Font selection done but need to add images to show which size has been selected Birthday selection done DP selection semi done (can select from gallery but camera not currently working)

--- app/src/main/java/com/itproject/frapp/ChooseFontSetting.java
+++ app/src/main/java/com/itproject/frapp/ChooseFontSetting.java
@@ -1,11 +1,15 @@
 package com.itproject.frapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.res.ResourcesCompat;
 
 import android.content.Intent;
+import android.content.res.Resources;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -20,7 +24,33 @@ public class ChooseFontSetting extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_choose_language);
+        setContentView(R.layout.activity_choose_font_setting);
+
+        // Authenticate current user
+        //mAuth = FirebaseAuth.getInstance();
+        //final FirebaseUser currentUser = mAuth.getCurrentUser();
+
+        // Connect to database
+        //dbRef = FirebaseDatabase.getInstance().getReference();
+
+        //Button next = findViewById(R.id.nextButton2);
+        //next.setOnClickListener(new View.OnClickListener() {
+        //    @Override
+        //    public void onClick(View view) {
+
+                // Set the location value for the user
+        //        String fontSize = "S"; // Replace this with whatever the user input is
+        //        dbRef.child("users").child(currentUser.getUid()).child("fontSize").setValue(fontSize);
+
+                // Move on the the next page - font settings
+        //        openBirthdaySetting();
+        //    }
+        //});
+    }
+
+
+    public void onClick(View view) {
+        String fontSize = "M";
 
         // Authenticate current user
         mAuth = FirebaseAuth.getInstance();
@@ -28,24 +58,34 @@ public class ChooseFontSetting extends AppCompatActivity {
 
         // Connect to database
         dbRef = FirebaseDatabase.getInstance().getReference();
+/*
+        switch (view.getId()) {
+            case R.id.smallButton:
+                fontSize = "S";
+                setImage(R.id.smallTickImage);
+                break;
+            case R.id.mediumButton:
+                fontSize = "M";
+                setImage(R.id.mediumTickImage);
+                break;
+            case R.id.largeButton:
+                fontSize = "L";
+                setImage(R.id.largeTickImage);
+                break;
+        }*/
 
-        Button next = findViewById(R.id.nextButton2);
-        next.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+        dbRef.child("users").child(currentUser.getUid()).child("fontSize").setValue(fontSize);
 
-                // Set the location value for the user
-                String fontSize = "S"; // Replace this with whatever the user input is
-                dbRef.child("users").child(currentUser.getUid()).child("fontSize").setValue(fontSize);
-
-                // Move on the the next page - font settings
-                openBirthdaySetting();
-            }
-        });
     }
 
-    public void openBirthdaySetting() {
-        Intent intent = new Intent(this, ChooseFontSetting.class);
+
+    private void setImage(int imageId) {
+        final ImageView imageView = (ImageView)findViewById(imageId);
+        //imageView.setImageResource(R.drawable.tick);
+    }
+
+    public void openBirthdaySetting(View view) {
+        Intent intent = new Intent(this, ChooseBirthdaySetting.class);
         startActivity(intent);
         finish();
     }
--- app/src/main/java/com/itproject/frapp/ChooseLanguageActivity.java
+++ app/src/main/java/com/itproject/frapp/ChooseLanguageActivity.java
@@ -5,23 +5,51 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
-public class ChooseLanguageActivity extends AppCompatActivity {
+public class ChooseLanguageActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     private FirebaseAuth mAuth;
     private DatabaseReference dbRef;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choose_language);
 
+
+        Spinner spinner = (Spinner) findViewById(R.id.languageSpinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.languageOptions, android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        //spinner.setPrompt("Select language");
+        spinner.setOnItemSelectedListener(this);
+
+        //Button next = findViewById(R.id.nextButton);
+        //next.setOnClickListener(new View.OnClickListener() {
+        //    @Override
+        //    public void onClick(View view) {
+
+                // Set the user's language
+        //        String language = "ENG"; // Replace this with user selection
+        //        dbRef.child("users").child(currentUser.getUid()).child("language").setValue(language);
+
+                // Move on the the next page - font settings
+               // openFontSizeSetting();
+        //    }
+        //});
+    }
+
+
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
         // Authenticate current user
         mAuth = FirebaseAuth.getInstance();
         final FirebaseUser currentUser = mAuth.getCurrentUser();
@@ -29,24 +57,24 @@ public class ChooseLanguageActivity extends AppCompatActivity {
         // Connect to database
         dbRef = FirebaseDatabase.getInstance().getReference();
 
-        Button next = findViewById(R.id.nextButton);
-        next.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+        String languageSelected = (String) parent.getItemAtPosition(position);
+        String language = "ENG";
 
-                // Set the user's language
-                String language = "ENG"; // Replace this with user selection
-                dbRef.child("users").child(currentUser.getUid()).child("language").setValue(language);
-
-                // Move on the the next page - font settings
-                openFontSizeSetting();
-            }
-        });
+        if (languageSelected.equals("English")) {
+            language = "ENG";
+        } else {
+            language = "CHN";
+        }
+        dbRef.child("users").child(currentUser.getUid()).child("language").setValue(language);
     }
 
-    public void openFontSizeSetting() {
+
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
+
+    public void openFontSizeSetting(View view) {
         Intent intent = new Intent(this, ChooseFontSetting.class);
         startActivity(intent);
-        finish();
     }
 }
