d02ac90b87aa700c591290a808b936b6cd7dc09f
mannb2 <ben.mann.2@gmail.com>
2019-10-06 18:36:16 +1100

    Images aren't cropped on artifact page, also cleaned up some code.

--- app/src/main/java/com/itproject/frapp/ArtifactAdapter.java
+++ app/src/main/java/com/itproject/frapp/ArtifactAdapter.java
@@ -1,7 +1,12 @@
+/* Team: frapp
+ * IT Project Semester 2, 2019
+ */
+
 package com.itproject.frapp;
 
 import android.app.Activity;
 import android.content.Context;
+import android.media.Image;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,6 +15,7 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
@@ -18,54 +24,58 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.ValueEventListener;
+import com.stfalcon.imageviewer.StfalconImageViewer;
+import com.stfalcon.imageviewer.loader.ImageLoader;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.Locale;
 
-
+// Adapter class for controlling the contents of the artifact page recycler view
 public class ArtifactAdapter extends RecyclerView.Adapter<ArtifactAdapter.ViewHolder> {
+
     private Artifact artifact;
     private String artifactID;
     private FirebaseUser currentUser;
     private DatabaseReference dbRef;
     private Context context;
 
-    public static class ViewHolder extends RecyclerView.ViewHolder {
-        public TextView op, dateTime, text;
-        public TextView date, description, tags;
-        public EditText newComment;
-        public Button postComment, back;
-        public ImageView image;
+    private final static int artifactViewType = 0;
+
+    // Class for each element of the recycler view (either the artifact + details or a comment)
+    static class ViewHolder extends RecyclerView.ViewHolder {
+        private TextView op, dateTime, text;
+        private TextView date, description, tags;
+        private EditText newComment;
+        private Button postComment, back;
+        private ImageView image;
 
 
-        public ViewHolder(View view, int viewType) {
+        ViewHolder(View view, int viewType) {
             super(view);
-            if (viewType == 0) {
-                this.image = (ImageView) view.findViewById(R.id.imageView_artifact);
-                this.date = (TextView) view.findViewById(R.id.textView_dateTime);
-                this.description = (TextView) view.findViewById(R.id.textView_description);
-                this.tags = (TextView) view.findViewById(R.id.textView_tags);
-                this.newComment = (EditText) view.findViewById(R.id.editText_newComment);
-                this.postComment = (Button) view.findViewById(R.id.button_postComment);
-                this.back = (Button) view.findViewById(R.id.button_back);
+            if (viewType == artifactViewType) {
+                this.image = view.findViewById(R.id.imageView_artifact);
+                this.date = view.findViewById(R.id.textView_dateTime);
+                this.description = view.findViewById(R.id.textView_description);
+                this.tags = view.findViewById(R.id.textView_tags);
+                this.newComment = view.findViewById(R.id.editText_newComment);
+                this.postComment = view.findViewById(R.id.button_postComment);
+                this.back = view.findViewById(R.id.button_back);
             } else {
-                this.op = (TextView) view.findViewById(R.id.textView_op);
-                this.dateTime = (TextView) view.findViewById(R.id.textView_dateTime);
-                this.text = (TextView) view.findViewById(R.id.textView_text);
+                this.op = view.findViewById(R.id.textView_op);
+                this.dateTime = view.findViewById(R.id.textView_dateTime);
+                this.text = view.findViewById(R.id.textView_text);
             }
         }
     }
 
+    @Override
     public int getItemViewType(int position) {
-        if (position == 0) {
-            return 0;
-        } else {
-            return position;
-        }
+        return position;
     }
 
-    public ArtifactAdapter(Artifact artifact, String artifactID, FirebaseUser currentUser, DatabaseReference dbRef, Context context) {
+    ArtifactAdapter(Artifact artifact, String artifactID, FirebaseUser currentUser, DatabaseReference dbRef, Context context) {
         this.artifact = artifact;
         this.artifactID = artifactID;
         this.currentUser = currentUser;
@@ -73,9 +83,11 @@ public class ArtifactAdapter extends RecyclerView.Adapter<ArtifactAdapter.ViewHo
         this.context = context;
     }
 
-    @Override
-    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        if (viewType == 0) {
+    // When a view holder is created, distinguish between the view for artifact details or for a comment
+    // using its view type
+    @Override @NonNull
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        if (viewType == artifactViewType) {
             View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.artifact, parent, false);
             return new ViewHolder(itemView, viewType);
         } else {
@@ -84,48 +96,51 @@ public class ArtifactAdapter extends RecyclerView.Adapter<ArtifactAdapter.ViewHo
         }
     }
 
+    // When a view holder is used, distinguish between the view for artifact details or for a comment
+    // using its position (view type)
     @Override
-    public void onBindViewHolder(final ViewHolder holder, int position) {
-        if (position == 0) {
+    public void onBindViewHolder(@NonNull final ViewHolder holder, int position) {
+        if (position == artifactViewType) {
             Glide.with(context)
                     .load(artifact.getUrl())
-                    .centerCrop()
+                    .fitCenter()
                     .into(holder.image);
             holder.date.setText(artifact.getDate());
             holder.description.setText(artifact.getDescription());
             holder.tags.setText(artifact.getTags());
 
+            // Posting a new comment
             holder.postComment.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
 
                     Date currentTime = Calendar.getInstance().getTime();
 
-                    SimpleDateFormat newDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+                    SimpleDateFormat newDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.US);
                     String dateText = newDateFormat.format(currentTime);
 
-//                    String dateText = currentTime.toString();
                     String newCommentText = holder.newComment.getText().toString();
-
 
                     Comment comment = new Comment();
                     comment.setOp(currentUser.getUid());
-                    System.out.println(currentUser.getDisplayName() + "test");
 
                     comment.setDateTime(dateText);
                     comment.setText(newCommentText);
-                    System.out.println(comment.toString() + "hello");
+
+                    // Push the new comment into the database
                     dbRef.child("artifacts").child(artifactID).child("comments").push().setValue(comment);
                 }
             });
 
-
+            // Pressing the back button to go back to the gallery
             holder.back.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
                     goBack();
                 }
             });
+
+
 
         } else {
             Comment comment = artifact.getComments().get(position - 1);
@@ -135,27 +150,33 @@ public class ArtifactAdapter extends RecyclerView.Adapter<ArtifactAdapter.ViewHo
 
             dbRef.child("users").child(comment.getOp()).addValueEventListener(new ValueEventListener() {
                 @Override
-                public void onDataChange(DataSnapshot dataSnapshot) {
+                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                     User user = dataSnapshot.getValue(User.class);
-                    holder.op.setText(user.getName());
+                    if (user != null) {
+                        holder.op.setText(user.getName());
+                    } else {
+                        holder.op.setText(context.getResources().getString(R.string.error));
+                    }
                 }
 
                 @Override
-                public void onCancelled(DatabaseError databaseError) {
+                public void onCancelled(@NonNull DatabaseError databaseError) {
                     // Something went wrong...
                     System.out.println("Error retrieving user data in comment");
-                    holder.op.setText("Error");
+                    holder.op.setText(context.getResources().getString(R.string.error));
                 }
             });
         }
     }
 
+    // Get the number of items that should be in the recycler view (number of comments + 1)
     @Override
     public int getItemCount() {
         return artifact.getComments().size() + 1;
     }
 
-    public void goBack() {
+    // Go back to the gallery
+    private void goBack() {
 //        Intent intent = new Intent(context, HomeActivity.class);
 //        context.startActivity(intent);
         ((Activity)context).finish();
--- app/src/main/java/com/itproject/frapp/User.java
+++ app/src/main/java/com/itproject/frapp/User.java
@@ -1,41 +1,47 @@
+/* Team: frapp
+ * IT Project Semester 2, 2019
+ */
+
 package com.itproject.frapp;
 
+// Class for storing and retrieving a user's name
 public class User {
 
-    private String email;
     private String name;
-    private String location;
 
-    public User() {
-    }
+//    private String email;
+//    private String location;
 
-    public User(String email, String fullName, String name, String location) {
-        this.email = email;
-        this.name = name;
-        this.location = location;
-    }
+//    public User() {
+//    }
+//
+//    public User(String email, String fullName, String name, String location) {
+//        this.email = email;
+//        this.name = name;
+//        this.location = location;
+//    }
 
     public String getName() {
         return name;
     }
 
-    public String getLocation() {
-        return location;
-    }
-
-    public String getEmail() {
-        return email;
-    }
+//    public String getLocation() {
+//        return location;
+//    }
+//
+//    public String getEmail() {
+//        return email;
+//    }
 
     public void setName(String name) {
         this.name = name;
     }
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public void setLocation(String location) {
-        this.location = location;
-    }
+//    public void setEmail(String email) {
+//        this.email = email;
+//    }
+//
+//    public void setLocation(String location) {
+//        this.location = location;
+//    }
 }
